d7c11c5a42d3778db1258447cc886a949649f301
Sarthak Sharma <sarthak312005@gmail.com>
2026-05-01 12:01:48 +0530

    cors fi

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const app = express();
 // Security
 app.use(helmet({ contentSecurityPolicy: false }));
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: "*",
   credentials: true,
 }));
 
